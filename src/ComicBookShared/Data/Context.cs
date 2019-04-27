4409d4a45e19aa113c87214fe7e5e3673da42e9d
hkoflax <hkoflax@users.noreply.github.com>
2019-04-26 23:43:01 -0400

    Finishing the Comic Book Artists Controller

--- src/ComicBookShared/Data/Context.cs
+++ src/ComicBookShared/Data/Context.cs
@@ -14,6 +14,7 @@ namespace ComicBookShared.Data
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Role> Roles { get; set; }
 
+        public DbSet<ComicBookArtist> ComicBookArtists { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             // Removing the pluralizing table name convention 
